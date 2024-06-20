f52a32aace16c5ea1f57504eb67b984e7e9a760a
rafeolll <rafael.y.paul@gmail.com>
2024-06-20 14:01:41 -0300

    vc

--- App.js
+++ App.js
@@ -1,8 +1,53 @@
-import AppNavigator from "./src/navigation/AppNavigator";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permissão de acesso à localização negada');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  const getLocation = async () => {
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  };
+
+  let text = 'Aguardando...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = `Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}`;
+  }
+
   return (
-    <AppNavigator/>
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+      <Button title="Atualizar Localização" onPress={getLocation} />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
 
